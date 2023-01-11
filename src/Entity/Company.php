b3d7d7de00ef29539f2474e99bf031a8bfd8e9e5
ToloCrook <tolotraramaro@hotmail.fr>
2023-01-11 17:16:00 +0100

    pushing entities

---
+++ src/Entity/Company.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompanyRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CompanyRepository::class)]
+class Company
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $numberVehicles = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $agencyLocation = null;
+
+    #[ORM\OneToMany(mappedBy: 'company', targetEntity: Vehicle::class)]
+    private Collection $vehicles;
+
+    public function __construct()
+    {
+        $this->vehicles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumberVehicles(): ?int
+    {
+        return $this->numberVehicles;
+    }
+
+    public function setNumberVehicles(?int $numberVehicles): self
+    {
+        $this->numberVehicles = $numberVehicles;
+
+        return $this;
+    }
+
+    public function getAgencyLocation(): ?string
+    {
+        return $this->agencyLocation;
+    }
+
+    public function setAgencyLocation(?string $agencyLocation): self
+    {
+        $this->agencyLocation = $agencyLocation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Vehicle>
+     */
+    public function getVehicles(): Collection
+    {
+        return $this->vehicles;
+    }
+
+    public function addVehicle(Vehicle $vehicle): self
+    {
+        if (!$this->vehicles->contains($vehicle)) {
+            $this->vehicles->add($vehicle);
+            $vehicle->setCompany($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVehicle(Vehicle $vehicle): self
+    {
+        if ($this->vehicles->removeElement($vehicle)) {
+            // set the owning side to null (unless already changed)
+            if ($vehicle->getCompany() === $this) {
+                $vehicle->setCompany(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehicleRepository::class)]
+class Vehicle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vehicleBrand = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vehicleModel = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vehicleType = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vehicleGearBoxType = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $vehicleSeatCount = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $vehicleDoorCount = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vehicleFuelType = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $year = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $status = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $price = null;
+
+    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'vehicle')]
+    private Collection $users;
+
+    #[ORM\ManyToOne(inversedBy: 'vehicles')]
+    private ?Company $company = null;
+
+    #[ORM\OneToMany(mappedBy: 'vehicle', targetEntity: Rental::class)]
+    private Collection $rentals;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+        $this->rentals = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVehicleBrand(): ?string
+    {
+        return $this->vehicleBrand;
+    }
+
+    public function setVehicleBrand(?string $vehicleBrand): self
+    {
+        $this->vehicleBrand = $vehicleBrand;
+
+        return $this;
+    }
+
+    public function getVehicleModel(): ?string
+    {
+        return $this->vehicleModel;
+    }
+
+    public function setVehicleModel(?string $vehicleModel): self
+    {
+        $this->vehicleModel = $vehicleModel;
+
+        return $this;
+    }
+
+    public function getVehicleType(): ?string
+    {
+        return $this->vehicleType;
+    }
+
+    public function setVehicleType(?string $vehicleType): self
+    {
+        $this->vehicleType = $vehicleType;
+
+        return $this;
+    }
+
+    public function getVehicleGearBoxType(): ?string
+    {
+        return $this->vehicleGearBoxType;
+    }
+
+    public function setVehicleGearBoxType(?string $vehicleGearBoxType): self
+    {
+        $this->vehicleGearBoxType = $vehicleGearBoxType;
+
+        return $this;
+    }
+
+    public function getVehicleSeatCount(): ?int
+    {
+        return $this->vehicleSeatCount;
+    }
+
+    public function setVehicleSeatCount(?int $vehicleSeatCount): self
+    {
+        $this->vehicleSeatCount = $vehicleSeatCount;
+
+        return $this;
+    }
+
+    public function getVehicleDoorCount(): ?int
+    {
+        return $this->vehicleDoorCount;
+    }
+
+    public function setVehicleDoorCount(?int $vehicleDoorCount): self
+    {
+        $this->vehicleDoorCount = $vehicleDoorCount;
+
+        return $this;
+    }
+
+    public function getVehicleFuelType(): ?string
+    {
+        return $this->vehicleFuelType;
+    }
+
+    public function setVehicleFuelType(?string $vehicleFuelType): self
+    {
+        $this->vehicleFuelType = $vehicleFuelType;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(?int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+            $user->addVehicle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            $user->removeVehicle($this);
+        }
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Rental>
+     */
+    public function getRentals(): Collection
+    {
+        return $this->rentals;
+    }
+
+    public function addRental(Rental $rental): self
+    {
+        if (!$this->rentals->contains($rental)) {
+            $this->rentals->add($rental);
+            $rental->setVehicle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRental(Rental $rental): self
+    {
+        if ($this->rentals->removeElement($rental)) {
+            // set the owning side to null (unless already changed)
+            if ($rental->getVehicle() === $this) {
+                $rental->setVehicle(null);
+            }
+        }
+
+        return $this;
+    }
+}
