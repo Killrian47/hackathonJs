9824afce637551959f1a64a9328db5fd889abfff
ToloCrook <tolotraramaro@hotmail.fr>
2023-01-12 17:27:09 +0100

    points reward Service system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,40 +16,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(UserRepository $userRepository, VehicleRepository $vehicleRepository, RentalRepository $rentalRepository): Response
+    public function index(): Response
     {
-        $rentals = $rentalRepository->findAll();
-        $users = $userRepository->findAll();
-        $vehicles = $vehicleRepository->findAll();
-
-        foreach ($rentals as $rental) {
-            $vehiclesRented = $rental->getVehicle();
-            $vehicleFuelType = $vehiclesRented->getVehicleFuelType();
-            $vehicleRentedId = $vehiclesRented->getId();
-            $renters = $rental->getRenter();
-            $renterId = $renters->getId();
-            foreach ($users as $user) {
-                $userId = $user->getId();
-                if ($renterId === $userId) {
-                    foreach ($vehicles as $vehicle) {
-                    $vehicleId = $vehicle->getId();
-                        if ($vehicleId === $vehicleRentedId) {
-                            if ($vehicleFuelType === 'electric') {
-                                $users = new User();
-                                $users->setPoints($users->getPoints() + 10);
-                         }
-                            if ($vehicleFuelType === 'hybrid') {
-                             $users->setPoints($users->getPoints() + 5);
-                            }
-                            $userRepository->save($users, true);
-                        }
-
-                        }
-                    }
-                var_dump($users->getPoints());
-            }
-        }
-        die();
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
