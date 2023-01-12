5c5abf87191450ccdf0384120bf6a16ba21a227b
yazidham <79407250+yazidham@users.noreply.github.com>
2023-01-12 10:58:38 +0100

    added the filter waiting the template for the integration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\VehicleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        //handle ajax request
+//        if ($request->isXmlHttpRequest()) {
+//            return new JsonResponse([
+//                'content' => $this->renderView('_includes/_vehicles_cards.html.twig', [
+//                    'vehicles' => $vehicleRepository->findAll(),
+//                ])
+//            ]);
+//        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+
+        ]);
+    }
+
+    #[Route('/vehicles/{type}/{sort}', name: 'app_vehicles')]
+    public function showVehicles(Request $request, VehicleRepository $vehicleRepository, string $type = 'all', string|int $sort = 'unsorted'): Response
+    {
+        if ($type !== 'all' && $sort !== 'unsorted') {
+            return $this->render('home/index.html.twig', [
+                'vehicles' => $vehicleRepository->findBy([$type => $sort])
+            ]);
+        }
+        return $this->render('home/index.html.twig', [
+            'vehicles' => $vehicleRepository->findAll()
         ]);
     }
 }
